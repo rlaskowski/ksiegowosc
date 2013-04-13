238ee895c9a61cd032028e374620fcb215c3995b
Rafał <>
2013-04-13 10:30:12 +0200

    Dodanie klasy Createser

---
+++ ksiegowosc-ejb/src/main/java/pl/ejb/user/CreateUser.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.ejb.user;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author Rafał
+ */
+@Stateless
+@LocalBean
+public class CreateUser {
+
+    
+
+}
